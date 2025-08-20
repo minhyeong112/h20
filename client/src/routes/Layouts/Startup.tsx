56afe1b8e60a5c830b12f8504cd006bf3f9fac80
Mig <104501046+minhyeong112@users.noreply.github.com>
2025-08-20 08:13:26 +0000

    vajra title fix

--- client/src/routes/Layouts/Startup.tsx
+++ client/src/routes/Layouts/Startup.tsx
@@ -38,7 +38,7 @@ export default function StartupLayout({ isAuthenticated }: { isAuthenticated?: b
   }, [isAuthenticated, navigate, data]);
 
   useEffect(() => {
-    document.title = startupConfig?.appTitle || 'H20';
+    document.title = startupConfig?.appTitle || 'Vajra';
   }, [startupConfig?.appTitle]);
 
   useEffect(() => {
